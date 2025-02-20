77eba2d9c080eb970fc868d19de875d0c8a2f4e0
BugHunter-X <askzenai@gmail.com>
2025-02-20 22:04:06 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,11 @@
+```solidity
+function transfer(address to, uint amount) public returns (bool success) {
+  require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+  require(to != address(0), "Transfer to the zero address");
+
+  balanceOf[msg.sender] -= amount;
+  balanceOf[to] += amount;
+  emit Transfer(msg.sender, to, amount);
+  return true;
+}
+```
